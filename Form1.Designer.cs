2437d834df4b828561c4b868b7fd55a4dbb83888
Anastasiia000 <57679919+Anastasiia000@users.noreply.github.com>
2020-05-26 16:15:39 +0300

    we have interface, conect to database

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,325 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelEkran = new System.Windows.Forms.Panel();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.btnChekID = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelPIN = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelEkran.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelEkran
+            // 
+            this.panelEkran.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(230)))), ((int)(((byte)(229)))));
+            this.panelEkran.Controls.Add(this.textBox1);
+            this.panelEkran.Controls.Add(this.labelPIN);
+            this.panelEkran.Controls.Add(this.txtCustomerID);
+            this.panelEkran.Controls.Add(this.pictureBox1);
+            this.panelEkran.Location = new System.Drawing.Point(16, 34);
+            this.panelEkran.Name = "panelEkran";
+            this.panelEkran.Size = new System.Drawing.Size(556, 340);
+            this.panelEkran.TabIndex = 0;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtCustomerID.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold);
+            this.txtCustomerID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(230)))), ((int)(((byte)(229)))));
+            this.txtCustomerID.Location = new System.Drawing.Point(141, 118);
+            this.txtCustomerID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCustomerID.MaxLength = 11;
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(260, 42);
+            this.txtCustomerID.TabIndex = 2;
+            this.txtCustomerID.Text = "Customer ID";
+            this.txtCustomerID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ATM.Properties.Resources.iconfinder_credit_card_3671752;
+            this.pictureBox1.Location = new System.Drawing.Point(105, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 355);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(171, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(228, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(284, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(171, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(228, 435);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(284, 435);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 49);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(171, 492);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 49);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(228, 492);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 49);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(284, 492);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 49);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button10.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(228, 548);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 49);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(255)))), ((int)(((byte)(58)))));
+            this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button11.Font = new System.Drawing.Font("Modern No. 20", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(339, 380);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(118, 49);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "CLEAR";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(92)))));
+            this.button12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button12.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(339, 435);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(118, 49);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "CANSEL";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(255)))), ((int)(((byte)(98)))));
+            this.button13.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button13.Font = new System.Drawing.Font("Modern No. 20", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(339, 492);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(118, 49);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "ENTER";
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // btnChekID
+            // 
+            this.btnChekID.BackgroundImage = global::ATM.Properties.Resources.iconfinder_card_inserting_309029;
+            this.btnChekID.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnChekID.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChekID.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChekID.Location = new System.Drawing.Point(579, 34);
+            this.btnChekID.Name = "btnChekID";
+            this.btnChekID.Size = new System.Drawing.Size(188, 202);
+            this.btnChekID.TabIndex = 15;
+            this.btnChekID.UseVisualStyleBackColor = true;
+            this.btnChekID.Click += new System.EventHandler(this.btnChekID_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 548);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(463, 390);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(724, 84);
+            this.listBox1.TabIndex = 17;
+            // 
+            // labelPIN
+            // 
+            this.labelPIN.AutoSize = true;
+            this.labelPIN.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold);
+            this.labelPIN.Location = new System.Drawing.Point(169, 181);
+            this.labelPIN.Name = "labelPIN";
+            this.labelPIN.Size = new System.Drawing.Size(74, 34);
+            this.labelPIN.TabIndex = 3;
+            this.labelPIN.Text = "PIN";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Location = new System.Drawing.Point(172, 217);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(54)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(790, 617);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChekID);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panelEkran);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ATM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelEkran.ResumeLayout(false);
+            this.panelEkran.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelEkran;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button btnChekID;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelPIN;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.Data;  
+using System.Data.SqlClient; 
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,83 @@ namespace ATM
 {
     public partial class Form1 : Form
     {
+        
+        //  string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\vistula\\ATM\\ATM\\Users.mdf;Integrated Security=True";
+        void MyConnection()
+        {                    
+            SqlConnection myConnection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\vistula\\ATM\\ATM\\Users.mdf;Integrated Security=True");
+            myConnection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM [User] WHERE [PersonalID] = 11111111111", myConnection);
+            SqlDataReader reader = command.ExecuteReader();
+            
+        }
+        
         public Form1()
         {
             InitializeComponent();
+            List<string> inf1 = new List<string>();
+            label1.Text = "nvklndlk";
+            //MyConnection();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnChekID_Click(object sender, EventArgs e)
+        {
+            //connect to database
+            String CustomerID = txtCustomerID.Text;
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\vistula\ATM\ATM\Users.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            await sqlConnection.OpenAsync(); //відкриваємо зв'язок з базою
+            //SqlDataReader sqlReader = null; //омагає нам получати таблицю в табличному представленні (у вигляді таблиці)
+           
+            //прописуємо команду, яку ми відтворюємо по відношенню до бази даних
+            SqlCommand command = new SqlCommand("SELECT * FROM [User] WHERE [PersonalID] = @ID", sqlConnection);
+            //@ID - заглушки, щоб було важче взламати "банк"
+            command.Parameters.Add("@ID", SqlDbType.VarChar).Value = CustomerID;
+
+            DataTable table = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command; //вибрали потрібно команду, виконали її
+            adapter.Fill(table); //помістили всі значення в об'єкт table
+
+            if (table.Rows.Count > 0)
+                MessageBox.Show("Yes");
+            else
+                MessageBox.Show("No");
+
+
+            /*
+            try
+            {
+                sqlReader = await command.ExecuteReaderAsync();
+                while (await sqlReader.ReadAsync())
+                {
+                    listBox1.Items.Add(Convert.ToString(sqlReader["PersonalId"]) + " " +
+                        Convert.ToString(sqlReader["Password"]) + " " + Convert.ToString(sqlReader["Saldo"]));
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            } 
+            */
+
+        }
+
+        private  void Form1_Load(object sender, EventArgs e)
+        {
+         
         }
     }
 }
